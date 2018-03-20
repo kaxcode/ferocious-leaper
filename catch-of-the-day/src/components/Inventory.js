32b281cf0b58baf13f587897e4a0149448d69127
Kenia <keniavalladarez@gmail.com>
2018-03-20 18:07:28 -0400

    Creates a form and a addFish function that is passed down to add fishes.

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+  static propTypes = {
+    addFish: PropTypes.func.isRequired
+  };
+
   render() {
-    return <div className="inventory">Inventory!!!</div>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
   }
 }
 
