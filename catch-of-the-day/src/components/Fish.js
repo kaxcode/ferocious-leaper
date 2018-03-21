929984a35bc6a1f99d35f4997b0d03c32de48fd1
Kenia <keniavalladarez@gmail.com>
2018-03-21 11:48:41 -0400

    Prints out all the fishes and their details into the menu.

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+  static propTypes = {
+    details: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { image, name, price, desc } = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+      </li>
+    );
+  }
+}
+
+export default Fish;
