deb7ba4bc4311014e828e558a364c480cf1e528e
Kenia <keniavalladarez@gmail.com>
2018-03-21 15:26:15 -0400

    Adds logic to edit a fish.

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AddFishForm from './AddFishForm';
+import EditFishForm from './EditFishForm';
 
 class Inventory extends React.Component {
   static propTypes = {
+    fishes: PropTypes.object.isRequired,
+    updateFish: PropTypes.func.isRequired,
     addFish: PropTypes.func.isRequired,
     loadSampleFishes: PropTypes.func.isRequired
   };
@@ -12,6 +15,14 @@ class Inventory extends React.Component {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(key => (
+          <EditFishForm
+            key={key}
+            index={key}
+            fish={this.props.fishes[key]}
+            updateFish={this.props.updateFish}
+          />
+        ))}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>
           Load Sample Fishes
